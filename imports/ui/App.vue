f22a474eda7ea5019937b593d3e4a017b071129e
Francisco <javiermartincuenca@gmail.com>
2021-06-11 22:33:39 +0200

    Loader

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -2,23 +2,27 @@
   <v-app>
     <v-container>
       <h1>Hello world from vue</h1>
-      <v-btn color="primary" @click="openAlert">Open</v-btn>
+      <v-btn color="primary" @click="openAlert">Open alert</v-btn>
+      <v-btn color="success" @click="openLoader">Open loader</v-btn>
     </v-container>
     <alert-message ref="refAlertMessageTest"></alert-message>
+    <loader ref="refLoaderTest" />
   </v-app>
 </template>
 
 <script>
-import AlertMessage from './components/utilities/AlertMessage.vue';
+import AlertMessage from './components/utilities/Alerts/AlertMessage.vue';
+import Loader from './components/utilities/Loaders/Loader.vue';
 
 export default {
   name: 'App',
   components: {
     AlertMessage,
+    Loader,
   },
   methods: {
     openAlert() {
-      this.$refs.refAlertMessageTest.showAlertSimple('success', 'Test title');
+      // this.$refs.refAlertMessageTest.showAlertSimple('success', 'Test title');
       this.$refs.refAlertMessageTest.showAlertFull(
         'mdi-close-circle',
         'error',
@@ -28,6 +32,12 @@ export default {
         'right',
         'bottom'
       );
+    },
+    openLoader() {
+      this.$refs.refLoaderTest.activate();
+      setTimeout(() => {
+        this.$refs.refLoaderTest.deactivate();
+      }, 3000);
     },
   },
 };
---
+++ imports/ui/components/utilities/Loaders/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="loader" hide-overlay persistent width="300">
+    <v-card color="primary" dark>
+      <v-card-text>
+        {{ progressLabel }}
+        <v-progress-linear indeterminate color="white" class="mb-0" />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loader: false,
+      progressLabel: '',
+    };
+  },
+  methods: {
+    activate(progressLabel = 'Loading') {
+      this.loader = true;
+      this.progressLabel = progressLabel;
+    },
+    deactivate() {
+      this.loader = false;
+    },
+  },
+};
+</script>

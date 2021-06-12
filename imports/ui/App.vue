33813d6b8151e6610c9e43c305a4e603a0ccfb4e
Francisco <javiermartincuenca@gmail.com>
2021-06-12 13:57:17 +0200

    Modal

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -4,21 +4,39 @@
       <h1>Hello world from vue</h1>
       <v-btn color="primary" @click="openAlert">Open alert</v-btn>
       <v-btn color="success" @click="openLoader">Open loader</v-btn>
+      <v-btn color="error" @click="openModalRemove">Open modal</v-btn>
     </v-container>
     <alert-message ref="refAlertMessageTest"></alert-message>
     <loader ref="refLoaderTest" />
+    <modal-remove
+      ref="refModalRemove"
+      :modal-data="modalInfo"
+      @id_element="deleteUser"
+    />
   </v-app>
 </template>
 
 <script>
 import AlertMessage from './components/utilities/Alerts/AlertMessage.vue';
 import Loader from './components/utilities/Loaders/Loader.vue';
+import ModalRemove from './components/utilities/Modals/ModalRemove.vue';
 
 export default {
   name: 'App',
   components: {
     AlertMessage,
     Loader,
+    ModalRemove,
+  },
+  data() {
+    return {
+      modalInfo: {
+        title: 'Remove User',
+        message: 'Are you sure want to remove this user?',
+        _id: null,
+        element: {},
+      },
+    };
   },
   methods: {
     openAlert() {
@@ -38,6 +56,27 @@ export default {
       setTimeout(() => {
         this.$refs.refLoaderTest.deactivate();
       }, 3000);
+    },
+    openModalRemove() {
+      const user = {
+        _id: 'unique_id',
+        name: 'John',
+        lastname: 'Doe',
+        age: 30,
+        country: 'USA',
+      };
+
+      this.modalInfo = {
+        ...this.modalInfo,
+        _id: user._id,
+        element: user,
+        message: `Are you sure want to remove ${user.name}?`,
+      };
+
+      this.$refs.refModalRemove.dialog = true;
+    },
+    deleteUser(idUser) {
+      console.log('User id', idUser);
     },
   },
 };
---
+++ imports/ui/components/utilities/Modals/ModalRemove.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog id="modalRemove" max-width="500px" v-model="dialog">
+    <v-card>
+      <v-card-title class="headline">
+        <div class="title">{{ modalData.title }}</div>
+      </v-card-title>
+      <v-card-text>
+        {{ modalData.message }}
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="default" text @click="cancel"> Cancel </v-btn>
+        <v-btn color="error" depressed @click="removeElement"> Remove </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    modalData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  methods: {
+    cancel() {},
+    removeElement() {
+      this.$emit('id_element', this.modalData._id);
+      this.dialog = false;
+    },
+  },
+};
+</script>

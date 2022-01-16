b579961096303b51c1a35e95b565fa284080918a
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-01-16 19:35:59 +0100

    Add router

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import Vue from 'vue';
 import vuetify from '../../ui/plugins/vuetify';
+import router from '../../ui/router';
 
 import App from '/imports/ui/App';
 
 Meteor.startup(() => {
   new Vue({
     vuetify,
+    router,
     render: (h) => h(App),
   }).$mount('app');
 });
---
+++ imports/ui/routes/loginRoutes.js
@@ -0,0 +1,8 @@
+import AuthLayout from '../layouts/AuthLayout.vue';
+
+export default {
+  path: '/login',
+  components: {
+    allPageView: AuthLayout,
+  },
+};
